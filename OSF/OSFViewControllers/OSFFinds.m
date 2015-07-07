d408452fa48ff3be76bb912676f064bac8f63b03
wjszf <478043385@qq.com>
2015-07-08 00:11:02 +0800

    修改的背景颜色  添加了图片

---
+++ OSF/OSFViewControllers/OSFFinds.m
@@ -0,0 +1,37 @@
+//
+//  OSFFinds.m
+//  OSF
+//
+//  Created by 赵锋 on 15/7/7.
+//  Copyright (c) 2015年 赵锋. All rights reserved.
+//
+
+#import "OSFFinds.h"
+
+@interface OSFFinds ()
+
+@end
+
+@implementation OSFFinds
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ OSF/OSFViewControllers/OSFUser.h
@@ -0,0 +1,13 @@
+//
+//  OSFUser.h
+//  OSF
+//
+//  Created by 赵锋 on 15/7/7.
+//  Copyright (c) 2015年 赵锋. All rights reserved.
+//
+
+#import "OSFBaseViewController.h"
+
+@interface OSFUser : OSFBaseViewController
+
+@end
